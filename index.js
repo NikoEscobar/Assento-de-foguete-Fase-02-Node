67856e7abc1fcad750cda302be1150760b62182d
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2019-06-26 03:31:36 -0300

    CRUD волк

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 //Players
-const users = ["Player01", "Player02", "Player03"];
+const users = ["Player01", "Player02", "Player03", "Player04"];
 
 server.get("/server", (req, res) => {
   return res.send({ message: "Server on" });
@@ -21,6 +23,32 @@ server.get("/users/:index", (req, res) => {
   return res.json(users[index]);
 });
 
-server.listen(3000);
-
 // Request body = { "name": Lobo, "email": "lobo@mau.com.br"}
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+//Update Player name by index
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+//Delete Player by index
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
+
+server.listen(3000);
