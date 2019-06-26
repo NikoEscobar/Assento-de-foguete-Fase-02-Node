a0997efcc1447bb80fe8ac1d52499d58415d52c1
Níkolas Escobar <40873324+NikoEscobar@users.noreply.github.com>
2019-06-26 20:43:20 -0300

    Add simple Middlewares волк

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ server.use(express.json());
 //Players
 const users = ["Player01", "Player02", "Player03", "Player04"];
 
+//Global Middleware for logs
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Method: ${req.method}; URL: ${req.url}`);
+
+  next();
+
+  console.timeEnd("Request");
+});
+
+//Local Middleware
+function checkUserExists(req, res, next) {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "User name is required" });
+  }
+
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const { index } = req.params;
+  const user = users[index];
+
+  if (!user) {
+    return res.status(400).json({ error: "User does not exists" });
+  }
+
+  req.user = user;
+
+  return next();
+}
+
 server.get("/server", (req, res) => {
   return res.send({ message: "Server on" });
 });
@@ -14,17 +48,16 @@ server.get("/server", (req, res) => {
 // Query params = ?name=Wolf
 server.get("/user", (req, res) => {
   const { name } = req.query;
-  return res.json({ message: `${name} Profile ` });
+  return res.json({ message: `${name} Profile` });
 });
 
 // Route params = /users/1
-server.get("/users/:index", (req, res) => {
-  const { index } = req.params;
-  return res.json(users[index]);
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  return res.json(req.user);
 });
 
 // Request body = { "name": Lobo, "email": "lobo@mau.com.br"}
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
 
   users.push(name);
@@ -33,7 +66,7 @@ server.post("/users", (req, res) => {
 });
 
 //Update Player name by index
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -43,7 +76,7 @@ server.put("/users/:index", (req, res) => {
 });
 
 //Delete Player by index
-server.delete("/users/:index", (req, res) => {
+server.delete("/users/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
